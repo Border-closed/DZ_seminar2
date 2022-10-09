749a8d124dc053d1ec4d080c1084013ae5a78e33
Dmitrii <directsdi@gmail.com>
2022-10-09 20:55:46 +0500

    вывод второй цифры из 3-х значного числа

---
+++ task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ВВедите трехзначное число:   ");
+int A = int.Parse (Console.ReadLine());
+int first = (A / 100);
+int third = A % 10;
+int second = (A - first*100 - third)/10;
+Console.WriteLine (second);
+
+
