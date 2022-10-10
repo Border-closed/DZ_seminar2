18bd684c9b517f161c249ae7c7e59dc84a7286d4
Dmitrii <directsdi@gmail.com>
2022-10-10 19:38:17 +0500

    вывод третьего символа из числа лобо текста, что третьего символа нет

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:   ");
+string A = Console.ReadLine();
+int length = A.Length;
+if (length>2)
+{
+    string B = A.Substring (0,3);
+    int C = int.Parse(B);
+    int third = C % 10;
+    Console.WriteLine (third);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет (=введено число меньше 100)");
+}
