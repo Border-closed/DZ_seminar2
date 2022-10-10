8d7085ca6c5a19747c4dc5aaac796a371eb7ec2f
Dmitrii <directsdi@gmail.com>
2022-10-10 20:00:20 +0500

    вывод выходной/не выходной/ошибка по цифре дня недели от пользователя

---
+++ task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число от 1 до 7, обозначающее день недели:   ");
+string A = Console.ReadLine();
+try
+{
+int B = int.Parse(A);
+if (B<0 || B>7)
+{
+    Console.WriteLine ("Вы ввели некорректное число. Введите число от 1 до 7, обозначающее день недели:   ");
+}
+    else if (B==6 || B==7)
+    { 
+        Console.WriteLine ("Это выходной");
+    }
+    else 
+    {
+        Console.WriteLine ("Это рабочий день");
+    }
+}
+catch
+{
+    Console.WriteLine ("Вы ввели текст. Введите число от 1 до 7, обозначающее день недели:   ");
+}
